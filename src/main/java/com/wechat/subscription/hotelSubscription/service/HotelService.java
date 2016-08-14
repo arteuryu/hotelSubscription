a58e31de7a4f4c404394e5d52ab44445ffa24eeb
arteuryu <yet341@gmail.com>
2016-08-14 14:15:14 +0800

    change pom.xml

---
+++ src/main/java/com/wechat/subscription/hotelSubscription/service/HotelService.java
@@ -0,0 +1,15 @@
+package com.wechat.subscription.hotelSubscription.service;
+
+import com.wechat.subscription.hotelSubscription.dao.entity.HotelInfo;
+
+public interface HotelService {
+	/**   
+	* @Title: HotelService.java 
+	* @Package com.wechat.subscription.hotelSubscription.service 
+	* @Description: TODO
+	* @author yuerte969  
+	* @date 14 Aug 2016 1:23:17 pm 
+	* @version V1.0   
+	*/
+	public HotelInfo testService();
+}
---
+++ src/main/java/com/wechat/subscription/hotelSubscription/service/impl/HotelServiceImpl.java
@@ -0,0 +1,13 @@
+package com.wechat.subscription.hotelSubscription.service.impl;
+
+import com.wechat.subscription.hotelSubscription.dao.entity.HotelInfo;
+import com.wechat.subscription.hotelSubscription.service.HotelService;
+
+public class HotelServiceImpl implements HotelService{
+
+	public HotelInfo testService() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
